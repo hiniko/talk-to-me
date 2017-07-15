eb485480a164b9e9529e91c1cf04da9c13ac384f
sherman Rose <sherman@shermanrose.uk>
2017-07-15 13:16:29 +0100

    moved connection identifcation code to the right place

--- app/channels/application_cable/channel.rb
+++ app/channels/application_cable/channel.rb
@@ -1,22 +1,4 @@
 module ApplicationCable
   class Channel < ActionCable::Channel::Base
-    identified_by :current_user
-
-    def connect
-      self.current_user = verify_cookie
-    end
-
-    def disconnect 
-
-    end
-
-    private 
-
-    def verify_cookie
-      if cookies.signed[:id].blank?  
-         reject_unauthorized_connection 
-      end
-    end
-
   end
 end
