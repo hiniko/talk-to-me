0ccf3d69a500812eb48dd77ccef4ec21acf971e2
sherman Rose <sherman@shermanrose.uk>
2017-07-15 13:04:11 +0100

    Added signed cookie for current user identification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action do |controller|
+    # Check to see if a tracking cookie exists 
+    if cookies.signed[:id].blank?
+      id = SecureRandom.hex 24
+      cookies.signed[:id] = id
+      @current_user = id
+    end
+      
+  end
+
 end
